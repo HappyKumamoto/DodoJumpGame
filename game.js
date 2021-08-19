a74d6b09a733fdd00d56ab3d537ea8b8c72c85f1
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-08-19 23:10:33 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,44 +1,45 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');//決まった文言
 const imageNames = ['tori', 'kinoko', 'saboten','moai'];
-const onclickArea = document.getElementById('onclick-area');//buttonをタップするため
+const onclickArea = document.getElementById('onclick-area');
 
-// グローバルな game オブジェクト
+
+// グローバルな game オブジェクト(グローバル変数)
 const game = {
-    counter: 0,
-    bgm: new Audio('bgm/jump.mp3'),
-    enemys: [],
-    image: {},
+    counter: 0,//何フレーム経過したか
+    //bgm: new Audio('bgm/jump.mp3'),
+    enemys: [],//敵キャラクター
+    image: {},//どれも最初は空っぽ
     isGameOver: true,
     score: 0,
-    timer: null
+    timer: null//切り替えタイマー
 };
 
 // 複数画像読み込み
-let imageLoadCounter = 0;
+let imageLoadCounter = 0;//最初は完了していない
 for (const imageName of imageNames) {
-    const imagePath = `image/${imageName}.png`;
+    const imagePath = `image/${imageName}.png`;//パス
     game.image[imageName] = new Image();
     game.image[imageName].src = imagePath;
     game.image[imageName].onload = () => {
-        imageLoadCounter += 1;
-        if (imageLoadCounter === imageNames.length) {
+        imageLoadCounter += 1;//読み込んだらカウンターをプラスしていく
+        if (imageLoadCounter === imageNames.length) {//全て完了したら
             console.log('画像のロードが完了しました。');
             init();
         }
     }
 }
 
-function init() {
+function init() {//init関数
     game.counter    = 0;
     game.enemys     = [];
     game.isGameOver = false;
     game.score      = 0;
-    createTori();
-    game.timer = setInterval(ticker, 30);
+    createTori();//ドードーどり用のオブジェクトを作る
+    game.timer = setInterval(ticker, 30);//ticker関数を30m秒ごとに実行
 }
 
-function ticker() {
+function ticker() {//パラパラ漫画状に書いていく//時を刻むもの
      // 画面クリア
      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -75,8 +76,8 @@ function ticker() {
 
 function createTori() {
     game.tori = {
-        x: game.image.tori.width / 2,
-        y: canvas.height - game.image.tori.height / 2,
+        x: game.image.tori.width / 2,//座標 画像の中心
+        y: canvas.height - game.image.tori.height / 2,//高さの中心
         moveY: 0,
         width: game.image.tori.width,
         height: game.image.tori.height,
@@ -95,7 +96,7 @@ function createKinoko() {
     });
 }
 
-/*function createHato() {
+/*function createHato() {//ランダムな高さ
     const hatoY = Math.random() * (300 - game.image.hato.height) + 150;
     game.enemys.push({
         x: canvas.width + game.image.hato.width / 2,
@@ -131,11 +132,11 @@ function createSaboten() {
 
 function moveTori() {
     game.tori.y += game.tori.moveY;
-    if (game.tori.y >= canvas.height - game.tori.height / 2) {
+    if (game.tori.y >= canvas.height - game.tori.height / 2) {//着地したら止まる
         game.tori.y = canvas.height - game.tori.height / 2;
         game.tori.moveY = 0;
     } else {
-        game.tori.moveY += 3;
+        game.tori.moveY += 3;//着地するまでは3ずつ動く
     }
 }
 
@@ -143,15 +144,15 @@ function moveEnemys() {
     for (const enemy of game.enemys) {
         enemy.x += enemy.moveX;
     }
-    // 画面の外に出たキャラクターを配列から削除
+    // 画面の外に出たキャラクターを配列から削除//配列からfilterで条件に合うものだけ残す
     game.enemys = game.enemys.filter(enemy => enemy.x > -enemy.width);
-}
+}//各敵キャラクターについて
 
 function drawTori() {
     ctx.drawImage(game.image.tori, game.tori.x - game.tori.width / 2, game.tori.y - game.tori.height / 2);
 }
 
-function drawEnemys() {
+function drawEnemys() {//ループさせる 
     for (const enemy of game.enemys) {
         ctx.drawImage(enemy.image, enemy.x - enemy.width / 2, enemy.y - enemy.height / 2);
     }
@@ -174,10 +175,11 @@ function hitCheck() {
             game.isGameOver = true;
             ctx.font = '25px Helvetica, Arial, sans-serif ';
             ctx.fillText(`＼エンター または リロードで再スタート／`, 150, 200);
-            clearInterval(game.timer);
+            clearInterval(game.timer);//動きが止まる
         }
     }
 }
+
 
 document.onkeydown = function(e) {
     /*if(e.key ===' '&& game.state === 'init'){
@@ -193,14 +195,8 @@ document.onkeydown = function(e) {
 };
 
 document.body.onclick = function(){
-    if(onclickArea === 'touched' && game.isGameOver === true){
-        init();
-    }
-    else{
     game.tori.moveY = -20;
-    }
 }
-
 
 
 
